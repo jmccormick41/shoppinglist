30d2ae37f18e90d0ea583244a2c72de373d7bd6d
jmccormick41 <jmccormick41@centurylink.net>
2016-03-17 05:56:22 -0600

    update

--- js/app.js
+++ js/app.js
@@ -1,28 +1,17 @@
-$(document).ready(function() {
-  $('#add').click(function() {
-
-    var userInput = $('input[name=shoppinglistitem]').val();
-
-    $('.list').append('<li>' + '<input class="checked-item" type="checkbox">' + userInput + "</li>");
-    return false;
-  });
-  
-        /****** toggle class ******/
-    
-    $(document).on("click",".checked-item",function(){
-    
-    $(this).parent().toggleClass( "it-is-checked");
-  });
-    
-   /***** End toggle class ******/
-    
-     $('.checked').on('click', function(e){
-      e.preventDefault(); 
-      $('.it-is-checked').remove();
-  });
-     
-     $('.reset-list').on('click', function(e) {
-       e.preventDefault();
-       $('li').remove();
-     }) 
+$(document).ready(function () {
+    var addItem = $('#addItem');
+    var removeItem = $('#remove');
+    addItem.click(function () {
+        var toAdd = $('input#text').val();
+        $('#shoppingList').append('<li><input type="checkbox"/>' + toAdd + '</li>');
+    });
+    removeItem.click(function () {
+        var toRemove = $('input#text').val();
+        $('li:contains('+toRemove+')').remove();
+    });
 });
+
+
+// check items off my list
+
+// delete items from my list
