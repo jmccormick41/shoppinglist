cfcdd095ea260d43209d80503e07f06c870a3fd0
jmccormick41 <jmccormick41@centurylink.net>
2016-03-24 06:13:03 -0600

    update

--- js/app.js
+++ js/app.js
@@ -1,55 +1,15 @@
-$(document).ready(function() {
-  /* add item on button click */
-  $("#AddButton").click(addItem);
-
-  /* add item when Enter key pressed when cursor is in textbox */
-  $("#newItemTextBox").keydown(function(event) {
-    var keycode = event.keyCode ? event.keyCode : event.which;
-    if(keycode == 13){
-      addItem();
-    }
-  });
-
-  /* remove item from list */
-  $("#list").on("click", "a", function() {
-    var listItem = $(this).closest("li");
-    var itemName = listItem.find("span").text();
-    var message = "Are you sure you want to remove '" + itemName + "' from the list?";
-    if(window.confirm(message)) {
-      listItem.remove();
-    }
-  });
-
-  /* strikethrough text if checkbox selected */
-  $("#list").on("change", "input:checkbox",function () {
-    var item = $(this).closest("li").find("span");
-    if($(this).is(":checked")) {
-      item.addClass("completed");
-      }
-    else
-      item.removeClass("completed"); 
-  });
-
-  /* add sorting to list */
-  $("#list").sortable({   
-    placeholder: "ui-sortable-placeholder" 
-    });  
+$(document).ready(function($) {
+    $('form').submit(function() {
+        if ($('.input').val() !== '') {
+            var newTask = $('.input').val();
+            var newLi = $('<li>' + newTask + '</li>');
+            newLi.on('click', function() {
+                $(this).remove(); // Attach the event handler *before* adding the element
+            });
+            $('ul').prepend(newLi); // To put the new task at the top of the list
+            $('.input').val('');
+            return false; // So the change persists
+        }
+    });
+    
 });
-
-function addItem() {
-  var newItem = $("#newItemTextBox").val();
-  if(newItem.length === 0) {
-    alert("You must enter an item to be added.");
-  }
-
-  var listItem = createListItem(newItem);
-  $("#list").append(listItem);
-  $("#newItemTextBox").val("");
-}
-
-function createListItem(newItem) {
-  var listItem = "<li class='ui-state-default'><input type='checkbox'>"; 
-  listItem += "<span>" + newItem + "</span>";
-  listItem += "<a href='#'>remove</a></li>";
-  return listItem; 
-}
